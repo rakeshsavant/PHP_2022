d27b88a0984ba3b622592f26160a67075bbad7fd
Rakesh <rakesh.r.savant@gmail.com>
2022-06-16 16:06:04 +0530

    Format corrected.

--- BasicHTML.php
+++ BasicHTML.php
@@ -1,77 +1,79 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-<p>Heading Element</p>
+    <p>Heading Element</p>
 
-<h1>H1</h1>
-<h2>H2</h2>
-<h3>H3</h3>
-<h4>H4</h5>
-<h6>H6</h6>
+    <h1>H1</h1>
+    <h2>H2</h2>
+    <h3>H3</h3>
+    <h4>H4</h5>
+        <h6>H6</h6>
 
-<p>Text Element :</p>
+        <p>Text Element :</p>
 
-<!-- <p>THis is paragraph tag</p> paragraph
+        <!-- <p>THis is paragraph tag</p> paragraph
 <br> line break
 <hr> horizontal rule -->
 
-<p>Physical Style </p>
+        <p>Physical Style </p>
 
-<b>Bold font</b> bold
-<i>Italic font effets</i> italic
+        <b>Bold font</b> bold
+        <i>Italic font effets</i> italic
 
-<p>Unordered (bullet) List </p>
+        <p>Unordered (bullet) List </p>
 
-<ul>
-<li>First Item</li>
-<li>Second Item</li>
-</ul>
+        <ul>
+            <li>First Item</li>
+            <li>Second Item</li>
+        </ul>
 
-<p>Ordered (Number) List</p>
+        <p>Ordered (Number) List</p>
 
-<ol>
-<li>First Item</li>
-<li>Second Item</li>
-</ol>
+        <ol>
+            <li>First Item</li>
+            <li>Second Item</li>
+        </ol>
 
-<p>Tables</p>
+        <p>Tables</p>
 
-<table border ="1">
-<tr>
-<th>some heading</th>
-<th>some heading</th>
-</tr>
-<tr>
-<td>sometext</td>
-<td>sometext</td>
-</tr>
-</table>
+        <table border="1">
+            <tr>
+                <th>some heading</th>
+                <th>some heading</th>
+            </tr>
+            <tr>
+                <td>sometext</td>
+                <td>sometext</td>
+            </tr>
+        </table>
 
-<p>Forms </p>
+        <p>Forms </p>
 
-<form action="" method="post">
+        <form action="" method="post">
 
-<p>Name:</p>
-<p><input type="text" name="name" value="Your name"></p>
+            <p>Name:</p>
+            <p><input type="text" name="name" value="Your name"></p>
 
-<p>Comments: </p>
-<p><textarea name="comments" rows="5" cols="20">Your comments</textarea></p>
+            <p>Comments: </p>
+            <p><textarea name="comments" rows="5" cols="20">Your comments</textarea></p>
 
-<p>Gender:</p>
-<p><input type="radio" name="gender" value="male"> Male</p>
-<p><input type="radio" name="gender" value="female"> Female</p>
+            <p>Gender:</p>
+            <p><input type="radio" name="gender" value="male"> Male</p>
+            <p><input type="radio" name="gender" value="female"> Female</p>
 
-<p><input type="submit"></p>
+            <p><input type="submit"></p>
 
-</form>
+        </form>
 
-<!-- Image Element
+        <!-- Image Element
 
 <img src ="" />
 
@@ -79,4 +81,5 @@ Links
 
 <a href ="http://www.examples.com /"> This is a link </a>  -->
 </body>
+
 </html>
